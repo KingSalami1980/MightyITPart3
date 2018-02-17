a6fea9b3235b550382f9cc53e836386153555b80
KingSalami1980 <35668864+KingSalami1980@users.noreply.github.com>
2018-02-17 13:47:40 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Part3
+{
+    public static class StringExtender
+    {
+
+        public static string Truncate(this string source, int length)
+        {
+            //If source is null, return null.
+            if (source.Length > length)
+            {
+                source = source.Substring(0, length);
+            }
+            return source;
+        }
+    }
+
+}
